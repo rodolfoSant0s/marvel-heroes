f693c561ae459a970e46ba50452238cbb6b64044
Rodolfo lucas dos santos <r.lucaazz@gmail.com>
2021-07-11 17:42:43 -0300

    Update Header.vue

--- front_end/src/components/Header.vue
+++ front_end/src/components/Header.vue
@@ -2,7 +2,7 @@
     <div>
         <img alt="King Host" src="https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2018/05/kinghost-logo-alt.png">
         <h1>Avaliação King Host</h1>
-        <h2>Rodolfo Santos - <a target="_blank" href="https://github.com/rodolfoSant0s"> Github</a></h2>        
+        <h2>Rodolfo Santos - <a target="_blank" href="https://github.com/rodolfoSant0s/marvel-heroes"> Github</a></h2>        
     </div>
 </template>
 <script>
