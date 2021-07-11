b0afabed6063466d8109abdc8195c4f71e026b1b
Rodolfo lucas dos santos <r.lucaazz@gmail.com>
2021-07-11 17:40:13 -0300

    Update index.js

--- front_end/src/utils/index.js
+++ front_end/src/utils/index.js
@@ -1,6 +1,6 @@
 
 const utils = {
-    base_url: process.env.NODE_ENV === 'production' ? 'http://apimarvelrodolfo.ddns.net/' : 'http://localhost:8000/api/v1/',
+    base_url: process.env.NODE_ENV === 'production' ? 'http://apimarvelrodolfo.ddns.net/api/v1/' : 'http://localhost:8000/api/v1/',
     heroes_selected: [1009368, 1009664, 1009351] // Ids de Heroies originais
 }
 
